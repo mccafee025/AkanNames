fc3377bf06d331d51e7d68a041cabcb37160e63c
mccafee025 <ademaamanka@gmail.com>
2021-09-08 19:36:06 +0300

    include a function in button event listener, write a function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,50 @@
-const fName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-const mName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let fName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let mName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
+// function to get dateinput
+function getInputdata() {
+  let date1 = parseInt(document.getElementById("dateinput").value);
+  let date1 = new Date();
+  let month = date1.getMonth() +1;
+  let year = date1.getFullYear();
+  let day = date1.getDate();
+
+  let MM = month;
+  let YY = year;
+  let DD = day;
+
+  let CC = year.slice(0, 2);
+
+  let gender = document.getElementById("gender").value;
+  
+
+}
+
+// console log
+console.log(mm);
+
+
+  // validation 
+if (dateinput = "") {
+  alert("choose a date");
+  } else {
+  getInputdata();
+}
+
+if(gender === male) {
+  document.getElementById("well").innerHTML=`your name is + ${}`
+
+  } else if (gender === female) {
+  document.getElementById("well").innerHTML= `your name is ${}`
+  
+
+
+let dayOfTheWeek =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+
+
+
+  
+  
+  
 
 
